c0d374cece4959f8fde487c6e308acb666281cfa
Administrator <Administrator@WIN-S09DU0P86JK>
2024-06-20 16:38:37 +0300

    Добавьте файлы проекта.

---
+++ task10/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+class Program
+{
+    static async Task Main()
+    {
+        string apiKey = "YOUR_API_KEY"; // Замените YOUR_API_KEY на ваш собственный ключ API от OpenWeatherMap
+        string city = "Moscow"; // Замените на название города, для которого хотите получить данные
+
+        using (var client = new HttpClient())
+        {
+            try
+            {
+                string url = $"http://api.openweathermap.org/data/2.5/weather?q={city}&appid={apiKey}&units=metric";
+                HttpResponseMessage response = await client.GetAsync(url);
+
+                if (response.IsSuccessStatusCode)
+                {
+                    string responseBody = await response.Content.ReadAsStringAsync();
+                    Console.WriteLine("Данные о погоде:");
+                    Console.WriteLine(responseBody);
+                }
+                else
+                {
+                    Console.WriteLine("Не удалось получить данные о погоде. Код ошибки: " + response.StatusCode);
+                }
+            }
+            catch (HttpRequestException e)
+            {
+                Console.WriteLine($"Ошибка при выполнении запроса: {e.Message}");
+            }
+        }
+    }
+}
